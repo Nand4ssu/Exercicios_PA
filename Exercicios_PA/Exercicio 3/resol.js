39060802d84f69e35eff8064aac10408e3ad442e
Nand.Devs <maaay.amanda@gmail.com>
2024-11-15 19:26:37 -0300

    Add files via upload

---
+++ Exercicios_PA/Exercicio 3/resol.js
@@ -0,0 +1,15 @@
+var letra = prompt("Digite F ou M para informar o seu gênero: ");
+
+
+var Feminino = "F";
+var Masculino = "M";
+
+
+if(letra === "F" ){
+    alert("O seu sexo é Feminino.");
+}
+ else if (letra === "M") {
+    alert("O seu sexo é Masculino");
+} else {
+    alert("Entrada inválida. Por favor, digite F ou M.");
+}
